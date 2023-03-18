769774f3991c96aebf46fc8a4c2aac5c6257804e
Richard Tichý <tichy@artify.zone>
2023-03-18 23:59:02 +0100

    Implement solution

--- src/ksp_35_z_4_3/core.clj
+++ src/ksp_35_z_4_3/core.clj
@@ -1,6 +1,30 @@
-(ns ksp-35-z-4-3.core)
+(ns ksp-35-z-4-3.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn read-input [file]
+  (let [[_ d & columns]
+        (-> file
+            slurp
+            (str/split #"(\ |\n)")
+            (#(map parse-long %)))]
+    [(dec d) (vec columns)]))
+
+(defn solve [d columns]
+  (->> columns
+       (map-indexed vector)
+       (take-while (fn [[i h]] (or (<= i d)
+                                   (<= h (nth columns d)))))
+       (map second)
+       reverse
+       (reduce (fn [[water top] curr]
+                 (if (<= top curr)
+                   [water curr]
+                   [(+ water (- top curr)) top]))
+               [0 (inc (nth columns d))])
+       first))
+
+(defn solve-file [input-file output-file]
+  (->> input-file
+       (read-input)
+       (apply solve)
+       (spit output-file)))
